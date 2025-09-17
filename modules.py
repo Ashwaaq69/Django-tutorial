bbd2d62ee94fd22949738e09f17e8b366739aab7
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-09-17 14:36:21 +0300

    modeles and pip in python

---
+++ modules.py
@@ -0,0 +1,8 @@
+# models in python 
+# modeulls allows you to get the function class or everything present innother file it allows you to implement it and those same functions in your own file or project. 
+
+import new
+
+new.say_hi()
+
+# pip allows you to install modelus from online your local
