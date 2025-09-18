5e03d98c4f23d7b597def9fcf221cabf9887080e
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-09-18 15:38:34 +0300

    Django installation and introduction of django

---
+++ introductionToDjango.py
@@ -0,0 +1,7 @@
+# Django is a high-level Python web framework that encourages rapid development and clean, pragmatic design. Built by experienced developers, it takes care of much of the hassle of web development, so you can focus on writing your app without needing to reinvent the wheel. It’s free and open source.
+
+#installed django using pip install django
+
+#installed virtual environment using pip install virtualenvwrapper-win  and created a virtual environment named 'myenv' and activated it using & "C:\Users\pc\Envs\myenv\Scripts\Activate.ps1 anc created file  django-admin startproject myproject"
+
+
